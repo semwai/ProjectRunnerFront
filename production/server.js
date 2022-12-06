12fbbda844817dd347387129461e6dcf2693123c
semwai <e14s@mail.ru>
2022-12-06 23:11:44 +0300

    logging client request

--- production/server.js
+++ production/server.js
@@ -10,6 +10,7 @@ const BUILD_FOLDER = "build";
 app.use(express.static(path.join(__dirname, BUILD_FOLDER)));
 
 app.get('/*', function (req, res) {
+    console.log(req.method, req.ip, req.url)
     res.sendFile(path.join(__dirname, BUILD_FOLDER, "index.html"));
 });
 
