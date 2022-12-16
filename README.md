dcdd5b0188c56605b1b49cde377dc5aac6e2153a
semwai <e14s@mail.ru>
2022-12-16 22:47:42 +0300

    #7 move monaco from cdn to bundle 0.6.2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ``` 
-docker buildx build --platform linux/amd64 -t semwai/front:0.6.0-amd64 .
-docker build -t semwai/front:0.6.0-arm64 .  
-docker push semwai/front:0.6.0-arm64
-docker push semwai/front:0.6.0-amd64
-docker run --rm -it -p 80:80 semwai/front:0.6.0-arm64
+docker buildx build --platform linux/amd64 -t semwai/front:0.6.2-amd64 .
+docker build -t semwai/front:0.6.2-arm64 .  
+docker push semwai/front:0.6.2-arm64
+docker push semwai/front:0.6.2-amd64
+docker run -d -p 80:80 semwai/front:0.6.2-arm64
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,37 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import {getProjects} from "./features/projects/projectsSlice";
 
+import * as monaco from "monaco-editor";
+import { loader } from "@monaco-editor/react";
+loader.config({ monaco });
+
+
+window.MonacoEnvironment = {
+    getWorker(moduleId, label) {
+        console.log(moduleId, label)
+        switch (label) {
+            case 'css':
+            case 'less':
+            case 'scss':
+                return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url));
+            case 'editorWorkerService':
+                return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url));
+            case 'handlebars':
+            case 'html':
+            case 'razor':
+                return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url));
+            case 'json':
+                return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url));
+            case 'javascript':
+            case 'typescript':
+                return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url));
+            //case 'yaml':
+            //    return new Worker(new URL('monaco-yaml/lib/esm/yaml.worker', import.meta.url));
+            default:
+                throw new Error(`Unknown label ${label}`);
+        }
+    },
+};
 
 
 const container = document.getElementById('root')!;
